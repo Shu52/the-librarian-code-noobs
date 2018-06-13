efa6457139772595b77391cf971cdbefec373b70
Joey <smithjoey5123@gmail.com>
2018-06-13 14:13:54 -0500

    names

--- customers.js
+++ customers.js
@@ -1,35 +1,101 @@
-const customerDatabase = [
-    {
-        'firstName': 'Emily',
-        'lastName':  'Venturino',
-        'genre':    'young adult',
-        'address': 'Nashville, TN',
-        'cardNumber': '',
-        'bookshelf': '',
-    
+const customerDatabase = []
+const customer = Object.create({}, {
+    firstName: {
+        value: '',
+        enumerable: true,
+        writable: true
     },
-    {
-        'firstName': 'Aaron',
-        'lastName':  'Miller    ',
-        'genre':    'sci-fi, philosophy, biography',
-        'address': 'Nashville, TN',
-        'cardNumber': '',
-        'bookshelf': '',
+    lastName: {
+        value: '',
+        enumerable: true,
+        writable: true
     },
-    {
-        'firstName': 'Joey',
-        'lastName':  'Smith',
-        'genre':    'horror, science fiction',
-        'address': 'Nashville, TN',
-        'cardNumber': '',
-        'bookshelf': '',
+    favoriteGenre: {
+        value: '',
+        enumerable: true,
+        writable: true
     },
-    {
-        'firstName': 'Lauren',
-        'lastName':  'Richert',
-        'genre':    'biography',
-        'address': 'Nashville, TN',
-        'cardNumber': '',
-        'bookshelf': '',
-    
-]
+    Address: {
+        value: '',
+        enumerable: true,
+        writable: true
+    },
+    cardNumber: {
+        value: 0,
+        enumerable: true,
+        writable: true
+    },
+    bookShelf: {
+        value: [],
+        enumerable: true,
+        writable: true
+    },
+    checkout: {
+        value: function () { }
+    },
+    checkIn: {
+        value: function () { }
+    }
+})
+
+
+const customerCreate = (fName, lName, Aress, cNum, fGen) => {
+    const person = Object.create(customer)
+
+    person.firstName = fName
+    person.lastName = lName
+    person.Address = Aress
+    person.cardNumber = cNum
+    person.favoriteGenre = fGen
+    customerDatabase.push(person)
+}
+
+customerCreate('Joey', 'Smith', '2323 erejje', 323432, 'horror')
+customerCreate('Emily', 'Venturino', '242134 jdkdsdf', 534343, 'young adult') 
+customerCreate('Aaron','Miller', '3243 dakfjdk', 534234, 'sci-fi')
+customerCreate('Lauren', 'Richert', '3432 dkjfdf', 64654, 'biography')
+console.log(customerDatabase);
+
+
+
+
+
+
+
+
+
+
+    // {
+    //     'firstName': 'Emily',
+    //     'lastName':  'Venturino',
+    //     'genre':    'young adult',
+    //     'address': 'Nashville, TN',
+    //     'cardNumber': '',
+    //     'bookshelf': '',
+
+    // },
+    // {
+    //     'firstName': 'Aaron',
+    //     'lastName':  'Miller    ',
+    //     'genre':    'sci-fi, philosophy, biography',
+    //     'address': 'Nashville, TN',
+    //     'cardNumber': '',
+    //     'bookshelf': '',
+    // },
+    // {
+    //     'firstName': 'Joey',
+    //     'lastName':  'Smith',
+    //     'genre':    'horror, science fiction',
+    //     'address': 'Nashville, TN',
+    //     'cardNumber': '',
+    //     'bookshelf': '',
+    // },
+    // {
+    //     'firstName': 'Lauren',
+    //     'lastName':  'Richert',
+    //     'genre':    'biography',
+    //     'address': 'Nashville, TN',
+    //     'cardNumber': '',
+    //     'bookshelf': '',
+
+    // }]
---
+++ librarian.js
@@ -0,0 +1,18 @@
+const librarian = Object.create({}, {
+    newCustomer: {
+        value: function (firstName, lastName, libraryNum, ) {
+            
+            const newCustomer = {
+                'firstName': firstName,
+                'lastName': lastName,
+                'cardNumber': cardNumber
+                
+            }
+         console.log(newCustomer);
+
+        
+            
+        }
+    }
+})
+
